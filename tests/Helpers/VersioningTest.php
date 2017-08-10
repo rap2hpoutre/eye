b53aeac9cf1e93d4135c2e046061c6a1528c53a4
Laurence <github@theshiftexchange.com>
2017-08-10 15:45:49 +0000

    more tests

--- tests/Helpers/VersioningTest.php
+++ tests/Helpers/VersioningTest.php
@@ -6,14 +6,14 @@ class VersioningTest extends TestCase
     {
         $pretend_version = '5.4.3';
 
-        // $this->assertFalse(laravel_version_is('>=', '5.5.0', $pretend_version));
-        // $this->assertFalse(laravel_version_is('>=', '5.4.4', $pretend_version));
-        // $this->assertTrue(laravel_version_is('>=', '5.4.3', $pretend_version));
-        // $this->assertTrue(laravel_version_is('>=', '5.3.0', $pretend_version));
+        $this->assertFalse(laravel_version_is('>=', '5.5.0', $pretend_version));
+        $this->assertFalse(laravel_version_is('>=', '5.4.4', $pretend_version));
+        $this->assertTrue(laravel_version_is('>=', '5.4.3', $pretend_version));
+        $this->assertTrue(laravel_version_is('>=', '5.3.0', $pretend_version));
 
-        // $this->assertFalse(laravel_version_is('>=', '5.5.1', $pretend_version));
-        // $this->assertTrue(laravel_version_is('>=', '4.2.4', $pretend_version));
-        // $this->assertTrue(laravel_version_is('>=', '5.4.10', '5.4.30'));
+        $this->assertFalse(laravel_version_is('>=', '5.5.1', $pretend_version));
+        $this->assertTrue(laravel_version_is('>=', '4.2.4', $pretend_version));
+        $this->assertTrue(laravel_version_is('>=', '5.4.10', '5.4.30'));
 
 
         $this->assertFalse(laravel_version_is('>=', '5.1.41', '5.1.40 (LTS)'));
